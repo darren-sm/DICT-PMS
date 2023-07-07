91748d3a7dd64f3bcdf4da6b35ad76bc1c2c7abd
Darren Malibiran <darrenmalibiran@outlook.com>
2023-07-07 12:09:49 +0800

    Get data only when user is logged in

--- web/myapp/views.py
+++ web/myapp/views.py
@@ -3,13 +3,13 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import CPMSForm, ExamineesForm, OJTInputForm
 from .models import CPMS, Examinees, OJTInput
+
 #homeview
 def home(request):
-    cpms_data = CPMS.objects.all()
-    examinees_data = Examinees.objects.all()
-    ojt_data = OJTInput.objects.all()
-
     if request.user.is_authenticated:
+        cpms_data = CPMS.objects.all()
+        examinees_data = Examinees.objects.all()
+        ojt_data = OJTInput.objects.all()
         return render(request, 'index.html', {
             'cpms_data': cpms_data,
             'examinees_data': examinees_data,
